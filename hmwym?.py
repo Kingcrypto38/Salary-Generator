dcfea17269b9c590801854b819d12aa397664dec
Euler <52365216+Eulerr@users.noreply.github.com>
2019-08-26 11:46:36 -0300

    Update hmwym?.py

--- hmwym?.py
+++ hmwym?.py
@@ -2,6 +2,7 @@ print('Money Calculator')
 y = float(input('How much money per hour? '))
 numbers = []
 while len(numbers) < 25:
+    #So you can't say you work 25 hours in a day
     number = float(input('How many hours per day? '))
     if not 1 <= number <= 24:
         print('only numbers between 1 and 24 are acceptable')
@@ -9,6 +10,7 @@ while len(numbers) < 25:
         z = float(input('How many days per year? '))
         qwerty = input('Do you pay taxes? ').lower()
         if 'yes'.startswith(qwerty.lower()):
+            #must answer with yes only
             a = float(input('Taxes are how much percent of your income? '))
             answer = (a/100)
             result = (number * y * z)
